d564b55d7e2f8d293c857b60a3c7bbfc23e8fbf7
Nityanand Singh <nns.nitya@gmail.com>
2024-02-02 17:22:20 +0530

    app setup: insertion done
    bean setup: HibernateTemplate, SessionFactory, DataSource, HibernateTransactionManager
    <tx:annotation-driven> added, @Transactional added in Student Dao, get/set added for hibernateTemplate

---
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -0,0 +1,28 @@
+package com.spring.orm.dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import com.spring.orm.entities.Student;
+
+public class StudentDao {
+
+	private HibernateTemplate hibernateTemplate;
+	
+	@Transactional
+	public int insert(Student student) {
+		//insert student
+		Integer i = (Integer)this.hibernateTemplate.save(student);
+		return i;
+	}
+
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
+	
+		
+}
